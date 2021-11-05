2c9ab1f15b9823d48d5b6da0a979c10c15d4797a
szkkng <zukkesukke23@gmail.com>
2021-11-05 15:08:38 +0900

    Clean up some code

--- Source/Components/RotarySlider.h
+++ Source/Components/RotarySlider.h
@@ -1,26 +1,22 @@
 #pragma once
 
 #include <JuceHeader.h>
-#include "CustomLookAndFeel.h"
+#include "../LookAndFeel/CustomLookAndFeel.h"
+#include "../LookAndFeel/MyColours.h"
 
 class RotarySlider  : public juce::Slider
 {
 public:
     RotarySlider();
     ~RotarySlider() override;
-    
+
     void paint (juce::Graphics& g) override;
-    
+
     void mouseDown (const juce::MouseEvent& event) override;
     void mouseUp (const juce::MouseEvent& event) override;
 
 private:
     CustomLookAndFeel customLookAndFeel;
-    
-    juce::Colour blue      = juce::Colour::fromFloatRGBA (0.43f, 0.83f, 1.0f, 1.0f);
-    juce::Colour grey      = juce::Colour::fromFloatRGBA (0.42f, 0.42f, 0.42f, 1.0f);
-    juce::Colour blackGrey = juce::Colour::fromFloatRGBA (0.2f,  0.2f,  0.2f, 1.0f);
-    juce::Colour offWhite  = juce::Colour::fromFloatRGBA (0.83f, 0.84f, 0.9f, 1.f);
-    
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RotarySlider)
 };
--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -12,41 +12,40 @@
 //==============================================================================
 SimpleReverbAudioProcessorEditor::SimpleReverbAudioProcessorEditor (SimpleReverbAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p),
-      sizeSliderAttachment (audioProcessor.apvts, "Room Size", sizeSlider),
-      dampSliderAttachment (audioProcessor.apvts, "Damping", dampSlider),
-      widthSliderAttachment (audioProcessor.apvts, "Width", widthSlider),
-      dwSliderAttachment (audioProcessor.apvts, "Dry/Wet", dwSlider),
-      freezeAttachment (audioProcessor.apvts, "Freeze", freezeButton)
+      sizeSliderAttachment  (audioProcessor.apvts, "size",    sizeSlider),
+      dampSliderAttachment  (audioProcessor.apvts, "damp",    dampSlider),
+      widthSliderAttachment (audioProcessor.apvts, "width",   widthSlider),
+      dwSliderAttachment    (audioProcessor.apvts, "dry/wet", dwSlider),
+      freezeAttachment      (audioProcessor.apvts, "freeze",  freezeButton)
 {
     juce::LookAndFeel::setDefaultLookAndFeel (&customLookAndFeel);
     setSize (500, 250);
     setWantsKeyboardFocus (true);
-    
+
     sizeLabel.setText ("size", juce::NotificationType::dontSendNotification);
     sizeLabel.attachToComponent (&sizeSlider, false);
-    
+
     dampLabel.setText ("damp", juce::NotificationType::dontSendNotification);
     dampLabel.attachToComponent (&dampSlider, false);
-    
+
     widthLabel.setText ("width", juce::NotificationType::dontSendNotification);
     widthLabel.attachToComponent (&widthSlider, false);
 
     dwLabel.setText ("dw", juce::NotificationType::dontSendNotification);
     dwLabel.attachToComponent (&dwSlider, false);
-    
+
     freezeButton.setButtonText (juce::String (juce::CharPointer_UTF8 ("∞")));
     freezeButton.setClickingTogglesState (true);
     freezeButton.setLookAndFeel (&customLookAndFeel);
     freezeButton.setColour (juce::TextButton::buttonColourId, juce::Colours::transparentWhite);
     freezeButton.setColour (juce::TextButton::buttonOnColourId, juce::Colours::transparentWhite);
-    freezeButton.setColour (juce::TextButton::textColourOnId, blue);
-    freezeButton.setColour (juce::TextButton::textColourOffId, grey);
+    freezeButton.setColour (juce::TextButton::textColourOnId, MyColours::blue);
+    freezeButton.setColour (juce::TextButton::textColourOffId, MyColours::grey);
 
     addAndMakeVisible (sizeSlider);
     addAndMakeVisible (dampSlider);
     addAndMakeVisible (widthSlider);
     addAndMakeVisible (dwSlider);
-   
     addAndMakeVisible (freezeButton);
 }
 
@@ -59,19 +58,19 @@ SimpleReverbAudioProcessorEditor::~SimpleReverbAudioProcessorEditor()
 //==============================================================================
 void SimpleReverbAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll (black);
-    
+    g.fillAll (MyColours::black);
+
     g.setFont (30);
-    g.setColour (offWhite);
+    g.setColour (MyColours::offWhite);
     g.drawText ("Simple Reverb", 150, 0, 200, 75, juce::Justification::centred);
 }
 
 void SimpleReverbAudioProcessorEditor::resized()
 {
-    sizeSlider.setBounds (30, 120, 60, 60);
-    dampSlider.setBounds (125, 120, 60, 60);
-    widthSlider.setBounds (315, 120, 60, 60);
-    dwSlider.setBounds (410, 120, 60, 60);
+    sizeSlider.setBounds   (30,  120, 60, 60);
+    dampSlider.setBounds   (125, 120, 60, 60);
+    widthSlider.setBounds  (315, 120, 60, 60);
+    dwSlider.setBounds     (410, 120, 60, 60);
     freezeButton.setBounds (210, 120, 80, 40);
 }
 
