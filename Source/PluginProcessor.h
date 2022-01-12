d8f5be024f9e093ffe22ecca6eaec2baeaf9fe6c
szkkng <zukkesukke23@gmail.com>
2022-01-12 11:33:13 +0900

    Use using decralation for APVTS

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,9 +53,9 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
-
-    juce::AudioProcessorValueTreeState apvts { *this, &undoManager, "Parameters", createParameterLayout() };
+    using APVTS = juce::AudioProcessorValueTreeState;
+    static APVTS::ParameterLayout createParameterLayout();
+    APVTS apvts { *this, &undoManager, "Parameters", createParameterLayout() };
 
 private:
     void updateReverbSettings();
