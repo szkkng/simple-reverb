c8b564aa936a489c7a6581e5150704260448ed3a
szkkng <zukkesukke23@gmail.com>
2022-01-18 21:48:10 +0900

    Changed the color of CornerResizer

--- Source/LookAndFeel/CustomLookAndFeel.cpp
+++ Source/LookAndFeel/CustomLookAndFeel.cpp
@@ -102,3 +102,27 @@ void CustomLookAndFeel::drawButtonBackground (juce::Graphics& g, juce::Button& b
                                               bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown)
 {
 }
+
+void CustomLookAndFeel::drawCornerResizer (juce::Graphics& g, int w, int h, bool /*isMouseOver*/, bool /*isMouseDragging*/)
+{
+    auto lineThickness = jmin ((float) w, (float) h) * 0.07f;
+
+    for (float i = 0.0f; i < 1.0f; i += 0.3f)
+    {
+        g.setColour (MyColours::blackGrey);
+
+        g.drawLine ((float) w * i,
+                    (float) h + 1.0f,
+                    (float) w + 1.0f,
+                    (float) h * i,
+                    lineThickness);
+
+        g.setColour (MyColours::blackGrey);
+
+        g.drawLine ((float) w * i + lineThickness,
+                    (float) h + 1.0f,
+                    (float) w + 1.0f,
+                    (float) h * i + lineThickness,
+                    lineThickness);
+    }
+}
