dbdaa4e4b74c3a47493e46aa4dc8bcc36d90fa57
szkkng <zukkesukke23@gmail.com>
2022-01-26 14:13:25 +0900

    Move CustomLabel to DialTextBox file

--- Source/GUI/MyLookAndFeel.h
+++ Source/GUI/MyLookAndFeel.h
@@ -4,45 +4,11 @@
 #include "../../Resources/AvenirNextMedium.h"
 #include "../../Resources/FuturaMedium.h"
 #include "MyColours.h"
+#include "DialTextBox.h"
 
 class MyLookAndFeel : public juce::LookAndFeel_V4
 {
 public:
-    struct CustomLabel : public juce::Label
-    {
-        static juce::String initValue;
-
-        void resized() override
-        {
-            juce::Label::resized();
-            setFont (getWidth() * 0.2f);
-        }
-
-        juce::TextEditor* createEditorComponent() override
-        {
-            auto* ed = juce::Label::createEditorComponent();
-
-            ed->setJustification (juce::Justification::centred);
-            ed->setColour (juce::TextEditor::backgroundColourId, juce::Colours::transparentWhite);
-            ed->setInputRestrictions (5, "0123456789.");
-            ed->setIndents (4, 0);
-
-            return ed;
-        }
-
-        void editorShown (juce::TextEditor* editor) override
-        {
-            getParentComponent()->setMouseCursor (juce::MouseCursor::NoCursor);
-            editor->clear();
-            editor->setText (initValue);
-        }
-
-        void editorAboutToBeHidden (juce::TextEditor * ed) override
-        {
-            getParentComponent()->setMouseCursor (juce::MouseCursor::NormalCursor);
-        }
-    };
-
     MyLookAndFeel();
 
     juce::Slider::SliderLayout getSliderLayout (juce::Slider& slider) override;
@@ -51,7 +17,7 @@ public:
                            float sliderPosProportional, float rotaryStartAngle,
                            float rotaryEndAngle, juce::Slider&) override;
 
-    CustomLabel* createSliderTextBox (juce::Slider& slider) override;
+    DialTextBox* createSliderTextBox (juce::Slider& slider) override;
 
     juce::CaretComponent* createCaretComponent (juce::Component* keyFocusOwner) override;
 
