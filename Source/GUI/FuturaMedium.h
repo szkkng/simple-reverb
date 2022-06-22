acbed4c31d059eda33b3756c76accc48a6c007be
szkkng <zukkesukke23@gmail.com>
2022-06-23 01:20:58 +0900

    Add the copyright and license notice

--- Source/GUI/FuturaMedium.h
+++ Source/GUI/FuturaMedium.h
@@ -21,34 +21,9 @@
 
 #pragma once
 
-#include <JuceHeader.h>
-#include "MyLookAndFeel.h"
-#include "MyColours.h"
-
-class Dial  : public juce::Slider
+namespace FuturaMedium
 {
-public:
-    Dial();
-    ~Dial() override;
+    extern const char*  FuturaMedium_ttf;
+    const int           FuturaMedium_ttfSize = 37516;
 
-    void paint (juce::Graphics& g) override;
-
-    void mouseEnter (const juce::MouseEvent& event) override;
-    void mouseExit (const juce::MouseEvent& event) override;
-    void mouseDown (const juce::MouseEvent& event) override;
-    void mouseDrag (const juce::MouseEvent& event) override;
-    void mouseUp (const juce::MouseEvent& event) override;
-
-    bool keyPressed (const juce::KeyPress& key) override;
-
-    void focusLost (FocusChangeType cause) override;
-
-private:
-    void drawFocusMark (juce::Graphics& g, juce::Colour colour);
-
-    juce::Time mouseUpTime;
-
-    MyLookAndFeel lnf;
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Dial)
-};
+}
